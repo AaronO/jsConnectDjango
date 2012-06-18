3713fc9fc114449e8ce4c327dbb29380bb664110
AaronO <aaron2013@gmail.com>
2012-06-18 11:02:43 +0200

    Fixed data dict ordering for signing

--- jsConnectDjango/helpers/sign.py
+++ jsConnectDjango/helpers/sign.py
@@ -1,10 +1,17 @@
 # Python imports
+from collections import OrderedDict
 from urllib import urlencode
 
 # Local imports
 from .hash import js_connect_hash
 
 def js_connect_sign(data, client_id, secret, hash_type, return_data = False):
+    old_data = data
+    keys = data.keys()
+    keys.sort()
+    data = OrderedDict.fromkeys(keys)
+    data.update(old_data)
+
     # Clean data
     for key in data:
         if not data[key]:
