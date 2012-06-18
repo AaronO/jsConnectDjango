03931865656eaba2a8c7d546c83bfd1e15404799
AaronO <aaron2013@gmail.com>
2012-06-18 11:29:37 +0200

    Added setup.py

---
+++ setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup, find_packages
+
+setup(
+    name = "Django JsConnect",
+    version = "0.1",
+    packages = find_packages(),
+    include_package_data = True,
+
+    author = "Aaron O'Mullane",
+    author_email = "aaron.omullan@gmail.com",
+    description = "Authentification bridge between a Django website and VanillaForums",
+    license = "Apache",
+    keywords = "python django authentication vanilla forums jsconnect",
+    url = "https://github.com/AaronO/jsConnectDjango",
+    install_requires = [],
+
+    classifiers = [
+        'Development Status :: 0.2 - Beta Testing',
+        'Environment :: Unix-like Systems',
+        'Intended Audience :: Developers, Project managers, Sys admins',
+        'Programming Language :: Python',
+        'Operating System :: Unix-like',
+    ],
+)
